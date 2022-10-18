62c7d5d3fe0c6d0354b60c3329200545f4251065
山D <dev@dyama.net>
2022-10-18 11:12:44 +0900

    basic1

--- basic1.c
+++ basic1.c
@@ -7,16 +7,49 @@
 #include "kernel_cfg.h"
 #endif
 
-#define sleep(ms) tslp_tsk(ms)
+// deca secs
+#define sleep(decs) tslp_tsk(decs * 100)
 
+#define mport_m EV3_PORT_A
 #define mport_l EV3_PORT_B
 #define mport_r EV3_PORT_C
+
+#define mmotor(p) ev3_motor_set_power(mport_m, p)
+#define lmotor(p) ev3_motor_set_power(mport_l, p)
+#define rmotor(p) ev3_motor_set_power(mport_r, p)
 #define steer(p, r) ev3_motor_steer(mport_l, mport_r, p, r)
+
+void stop() {
+  steer(10, 0);
+  sleep(1);
+  steer(0, 0);
+}
+
+void turn(int decs) {
+  lmotor(20);
+  rmotor(-20);
+  sleep(decs);
+}
+
+void pen_down() {
+  mmotor(8);
+  sleep(3);
+  mmotor(0);
+}
+
+void pen_up() {
+  mmotor(-8);
+  sleep(3);
+  mmotor(0);
+  sleep(2);
+}
 
 void main_task(intptr_t _) {
   // init
+  ev3_motor_config(mport_m, MEDIUM_MOTOR);
   ev3_motor_config(mport_l, LARGE_MOTOR);
   ev3_motor_config(mport_r, LARGE_MOTOR);
+  ev3_motor_reset_counts(mport_m);
   ev3_motor_reset_counts(mport_l);
   ev3_motor_reset_counts(mport_r);
 
@@ -24,7 +57,27 @@ void main_task(intptr_t _) {
 }
 
 void run_task(intptr_t _) {
-  steer(10, 0);
-  sleep(2000);
-  steer(0, 0);
+
+  //
+  pen_down();
+  lmotor(30);
+  rmotor(-30);
+  sleep(20);
+  stop();
+  pen_up();
+
+  //
+  steer(50, 0);
+  sleep(4);
+  stop();
+  turn(8);
+
+  //
+  stop();
+  pen_down();
+  lmotor(50);
+  rmotor(20);
+  sleep(45);
+  stop();
+  pen_up();
 }
